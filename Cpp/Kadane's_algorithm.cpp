e689bf2de88bd60f7e1b7389960389caced7575c
Soham <sohamdp28@gmail.com>
2020-10-18 21:31:43 +0530

    contiguous sub-array with maximum sum

--- Cpp/Kadane's_algorithm.cpp
+++ Cpp/Kadane's_algorithm.cpp
@@ -1,13 +1,4 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
-
 using namespace std;
 int Kadanesalgo(int array[],int size)
 {
